0d3bc4201770fd432709403a9195c80e5cddaaa2
emoacht <emotom@pobox.com>
2015-06-19 21:51:16 +0900

    Added extension method for StreamReader

---
+++ StringReaderCatalog/StreamReaderExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReaderCatalog
+{
+	/// <summary>
+	/// Extension method for <see cref="System.IO.StreamReader"/>
+	/// </summary>
+	public class StreamReaderExtension
+	{
+		/// <summary>
+		/// Read lines as IEnumerable.
+		/// </summary>
+		/// <param name="reader">StreamReader</param>
+		/// <returns>Content of lines</returns>
+		public static IEnumerable<string> EnumerateLines(this StreamReader reader)
+		{
+			while (!reader.EndOfStream)
+			{
+				yield return reader.ReadLine();
+			}
+		}
+	}
+}
